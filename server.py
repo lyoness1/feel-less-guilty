88a993754e6bc534ea3a6a3683f0ad4390c0c82f
lyoness1 <alyoness1@gmail.com>
2016-09-10 11:59:56 -0700

    bare bones

--- server.py
+++ server.py
@@ -13,12 +13,6 @@ import os
 import yelp_results
 import json
 
-############################################# IMPORTED MODEL TABLES TO ROUTES
-# from model import User, Event, Attraction, UserEvent, UserAttraction
-# from model import connect_to_db, db
-
-##############################################################################
-
 app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
@@ -28,9 +22,6 @@ app.config['SECRET_KEY'] = os.environ.get("FLASK_SECRET_KEY", "abcdef")
 # This is horrible. Fix this so that, instead, it raises an error.
 app.jinja_env.undefined = StrictUndefined
 
-##############################################################################
-##############################################################################
-							# HOME PAGE/SEARCH FIELD
 
 
 @app.route('/')
@@ -39,9 +30,6 @@ def index():
 
 	return render_template("homepage.html")
 
-##############################################################################
-##############################################################################
-							# SEARCH PROCESS AND RESULTS
 
 
 @app.route("/process_search", methods=["GET"])
@@ -60,32 +48,6 @@ def search_process():
 	return render_template("search_results.html", location=location,yelp_result=yelp_result)
 
 
-##############################################################################
-##############################################################################
-							# SEARCH PROCESS AND RESULTS
-
-@app.route('/twilio', methods=['POST'])
-def twilio():
- 
- 	message = request.form.get('message')
- 	
-	ACCOUNT_SID = "AC3a959eac16c2ec0874ea33ac53d6e021" 
-	AUTH_TOKEN = "264e6637c88819c5b5d6c4a4579c4e37" 
- 
-	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
- 
-	client.messages.create(
-		to="+17076960691", 
-		from_="+17073311083", 
-		body=message) 
-	
-	return redirect("/my_agenda")
-
-
-##############################################################################
-##############################################################################
-							# HELPER FUNCTIONS
-
 
 if __name__ == "__main__":
 	# We have to set debug=True here, since it has to be True at the point
@@ -100,8 +62,6 @@ if __name__ == "__main__":
 	
 	app.run(host="0.0.0.0", port=PORT, debug=False)
 
-
-	
 
 	# Use the DebugToolbar
 	# DebugToolbarExtension(app)
